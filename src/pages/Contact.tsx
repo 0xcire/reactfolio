dba1f0dc5436e54e5e72651ba6dcc4ecda9c366d
0xcire <ciredeveloper@gmail.com>
2023-03-14 16:10:31 -0400

    removed

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { useForm, SubmitHandler } from 'react-hook-form';
 import { string, z } from 'zod';
